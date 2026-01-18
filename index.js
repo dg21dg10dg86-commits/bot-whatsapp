9554c566706a1953dd1f939572d0e3fdd1ba67a2
Douglas Gabriel <DG21DG10DG86@gmail.com>
2026-01-17 23:56:07 -0300

    Configuração simplificada para Render

--- index.js
+++ index.js
@@ -637,18 +637,8 @@ const client = new Client({
     clientId: "bot-detran-render"
   }),
   puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
-    ],
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser'
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
 });
 
